b662e3c59aa4b28ef0d6f13d85d9fab79371f556
denismezhenin <dzianismiazhenin@gmail.com>
2022-08-05 19:32:44 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -38,7 +38,13 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayOfNumbers = []
+    for (let i = 0; i < expr.length; i += 10) {
+     arrayOfNumbers.push(expr.slice(i, i + 10))
+    }
+   let result = []
+   result = arrayOfNumbers.map((item) => item.replace(/00/gi, '')).map((item) => item.replace(/10/gi, '.')).map((item) => item.replace(/11/gi, '-')).map((item) => item.replace('**********', ' ')).map((item) => item == ' ' ? ' ' : MORSE_TABLE[item]).join('')
+   return result
 }
 
 module.exports = {
